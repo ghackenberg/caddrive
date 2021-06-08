a75f9dc5be731f0771bb907bb388800cc9826c17
Georg Hackenberg <ghackenberg@gmail.com>
2021-06-08 16:49:50 +0200

    do not ignore webpack config

---
+++ worker/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './bin/main.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'worker.js'
+    }
+}
