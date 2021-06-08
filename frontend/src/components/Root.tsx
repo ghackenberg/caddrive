c505d717813fdc52f1363ef651974e4488dc4643
Georg Hackenberg <ghackenberg@gmail.com>
2021-06-08 16:55:44 +0200

    add rest api client

--- frontend/src/components/Root.tsx
+++ frontend/src/components/Root.tsx
@@ -4,6 +4,7 @@ import { Scene, PerspectiveCamera, WebGLRenderer, PointLight, AmbientLight, sRGB
 import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory'
 import { VRButton } from 'three/examples/jsm/webxr/VRButton'
 import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { AuditAPI, ProductAPI, UserAPI } from '../api'
 
 export default class Root extends React.Component {
     private main: React.RefObject<HTMLElement>
@@ -90,6 +91,10 @@ export default class Root extends React.Component {
         window.addEventListener('resize', this.resize)
         // Resize
         setTimeout(this.resize, 100)
+        // API
+        console.log(await UserAPI.findAll())
+        console.log(await ProductAPI.findAll())
+        console.log(await AuditAPI.findAll())
     }
     
     componentWillUnmount() {
