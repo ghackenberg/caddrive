cd0d907c1c794415adcb5fd3ad2ebc40a26acad4
Georg Hackenberg <ghackenberg@gmail.com>
2021-08-04 17:19:32 +0200

    adjust titles

--- frontend/src/scripts/components/snippets/Header.tsx
+++ frontend/src/scripts/components/snippets/Header.tsx
@@ -5,7 +5,7 @@ import * as PlatformIcon from '/src/images/platform.png'
 export const Header = () => (
     <header>
         <Link to="/">
-            <img src={PlatformIcon}/> Virtual Engineering Platform
+            <img src={PlatformIcon}/> FHOOE Virtual Engineering Platform
         </Link>
     </header>
 )
