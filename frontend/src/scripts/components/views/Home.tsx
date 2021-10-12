e740a3af5d6d20f34aa64e098117b55fcc7170f1
Georg Hackenberg <ghackenberg@gmail.com>
2021-10-12 17:42:25 +0200

    add home link to home view

--- frontend/src/scripts/components/views/Home.tsx
+++ frontend/src/scripts/components/views/Home.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { HomeLink } from '../links/HomeLink'
 // Snippets
 import { Header } from '../snippets/Header'
 import { Navigation } from '../snippets/Navigation'
@@ -10,6 +11,9 @@ export const HomeView = () => (
         <Header/>
         <Navigation/>
         <main>
+            <nav>
+                <HomeLink/>
+            </nav>
             <h1>Home</h1>
             <h2>Welcome to the FH O&Ouml; VIRUTAL ENGINEERING PLATFORM !</h2>
             <p>Please choose between <Link to="/users">Users</Link> and <Link to="/products">Products</Link>.</p>
