60eee2014820da153f2a3752f60777e41d2fb8aa
DomFrue <s2110564009@fhooe.at>
2022-03-08 19:00:34 +0100

    added memberservice methodes

--- packages/backend/src/rest/members/member.service.ts
+++ packages/backend/src/rest/members/member.service.ts
@@ -1,20 +1,69 @@
 import { Member, MemberData, MemberREST } from 'productboard-common'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import * as shortid from 'shortid'
 
+
+
+@Injectable()
 export class MemberService implements MemberREST {
-    findMembers(_productId: string): Promise<Member[]> {
-        throw new Error('Method not implemented.');
+    private static readonly members: Member[] = [
+        { id: 'demo-1', userId: 'demo-1', productId: "demo-1"},
+        { id: 'demo-2', userId: 'demo-2', productId: "demo-2"}
+    ]
+    
+    public constructor() {}
+
+    async findMembers(productId: string): Promise<Member[]> {
+        const result: Member[] = []
+        for (const member of MemberService.members) {
+            if (member.productId != productId) {
+                continue
+            }
+            result.push(member)
+        }
+
+        return result
     }
-    addMember(_data: MemberData): Promise<Member> {
-        throw new Error('Method not implemented.');
+
+    async addMember(data: MemberData): Promise<Member> {
+        const member = { id: shortid(), ...data }
+        MemberService.members.push(member)
+        return member
     }
-    getMember(_id: string): Promise<Member> {
-        throw new Error('Method not implemented.');
+
+    async getMember(id: string): Promise<Member> {
+        for (const member of MemberService.members) {
+            if (member.id == id) {
+                return member
+            }
+        }
+        throw new NotFoundException()
     }
-    updateMember(_id: string, _data: MemberData): Promise<Member> {
-        throw new Error('Method not implemented.');
+
+    async updateMember(id: string, data: MemberData): Promise<Member> {
+        for (var index = 0; index < MemberService.members.length; index++) {
+            const member = MemberService.members[index]
+            if (member.id == id) {
+                MemberService.members.splice(index, 1, { id, ...data })
+                return MemberService.members[index]
+            }
+        }
+        throw new NotFoundException()
     }
-    deleteMember(_id: string): Promise<Member> {
-        throw new Error('Method not implemented.');
+    async deleteMember(id: string): Promise<Member> {
+        for (var index = 0; index < MemberService.members.length; index++) {
+            const member = MemberService.members[index]
+            if (member.id == id) {
+                // Todo
+                // for (const member of await this.MemberService.findMembers(id)) {
+                //     await this.MemberService.deleteMember(member.id)
+                // }
+                // member.deleted = true
+                return member
+            }
+        }
+        throw new NotFoundException()
     }
+    
 
 }
