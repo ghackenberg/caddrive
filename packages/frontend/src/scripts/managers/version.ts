3546b4de3895d4464839a39eca3cc8487d925ef8
Georg Hackenberg <ghackenberg@gmail.com>
2022-03-09 08:41:49 +0100

    implement caching version manager

--- packages/frontend/src/scripts/managers/version.ts
+++ packages/frontend/src/scripts/managers/version.ts
@@ -2,20 +2,85 @@ import { Version, VersionData, VersionREST } from 'productboard-common'
 import { VersionClient } from '../clients/rest/version'
 
 class VersionManagerImpl implements VersionREST<VersionData, File> {
+    private versionIndex: {[id: string]: Version} = {}
+    private productIndex: {[id: string]: {[id: string]: boolean}} = {}
+
     async findVersions(productId: string): Promise<Version[]> {
-        return VersionClient.findVersions(productId)
+        if (!(productId in this.productIndex)) {
+            // Contact backend
+            const versions = await VersionClient.findVersions(productId)
+            // Update version index
+            for (const version of versions) {
+                this.versionIndex[version.id] = version
+            }
+            // Update product index
+            this.productIndex[productId] = {}
+            for (const version of versions) {
+                this.productIndex[productId][version.id] = true
+            }
+        }
+        // Return versions
+        return Object.keys(this.productIndex[productId]).map(id => this.versionIndex[id])
     }
+
     async addVersion(data: VersionData, file: File): Promise<Version> {
-        return VersionClient.addVersion(data, file)
+        // Call backend
+        const version = await VersionClient.addVersion(data, file)
+        // Update version index
+        this.versionIndex[version.id] = version
+        // Update product index
+        if (version.productId in this.productIndex) {
+            this.productIndex[version.productId][version.id] = true
+        }
+        // Return version
+        return version
     }
+
     async getVersion(id: string): Promise<Version> {
-        return VersionClient.getVersion(id)
+        if (!(id in this.versionIndex)) {
+            // Call backend
+            const version = await VersionClient.getVersion(id)
+            // Update version index
+            this.versionIndex[id] = version
+            // Update product index
+            if (!(version.productId in this.productIndex)) {
+                this.productIndex[version.productId] = {}
+            }
+            this.productIndex[version.productId][id] = true
+        }
+        // Return version
+        return this.versionIndex[id]
     }
+
     async updateVersion(id: string, data: VersionData, file?: File): Promise<Version> {
-        return VersionClient.updateVersion(id, data, file)
+        // Update product index
+        if (id in this.versionIndex) {
+            this.productIndex[this.versionIndex[id].productId] = undefined
+        }
+        // Call backend
+        const version = await VersionClient.updateVersion(id, data, file)
+        // Update version index
+        this.versionIndex[id] = version
+        // Update product index
+        if (!(version.productId in this.productIndex)) {
+            this.productIndex[version.productId] = {}
+        }
+        this.productIndex[version.productId][id] = true
+        // Return version
+        return version
     }
+
     async deleteVersion(id: string): Promise<Version> {
-        return VersionClient.deleteVersion(id)
+        // Call backend
+        const version = await VersionClient.deleteVersion(id)
+        // Update version index
+        this.versionIndex[id] = version
+        // Update product index
+        if (version.productId in this.productIndex) {
+            this.productIndex[version.productId][id] = undefined
+        }
+        // Return version
+        return version
     }
 }
 
