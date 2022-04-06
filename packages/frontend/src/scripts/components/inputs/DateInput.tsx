4d95f12c1cdc95764eb83001da7a1780bb0865e4
Georg Hackenberg <ghackenberg@gmail.com>
2022-04-06 09:54:23 +0200

    clean up milestone view

--- packages/frontend/src/scripts/components/inputs/DateInput.tsx
+++ packages/frontend/src/scripts/components/inputs/DateInput.tsx
@@ -2,15 +2,15 @@ import * as React from 'react'
 // Inputs
 import { GenericInput } from './GenericInput'
 
-export const DateInput = (props: {class?: string, label: string, change: (value: Date) => void, value: Date, placeholder?: string, disabled?: boolean}) => (
+export const DateInput = (props: {class?: string, label: string, change: (value: Date) => void, value: Date, placeholder?: string, disabled?: boolean, required?: boolean}) => (
     <GenericInput label={props.label}>
         <input
             type='date'
             className={props.class}
             placeholder={props.placeholder}
             value={(props.value ? props.value : new Date()).toISOString().slice(0, 10)}
-            disabled={props.disabled}
             onChange={event => {props.change(event.currentTarget.valueAsDate)}}
-            required/>
+            disabled={props.disabled}
+            required={props.required}/>
     </GenericInput>
 )
--- packages/frontend/src/scripts/components/views/Milestone.tsx
+++ packages/frontend/src/scripts/components/views/Milestone.tsx
@@ -13,68 +13,56 @@ import { ProductView3D } from '../widgets/ProductView3D'
 import { TextInput } from '../inputs/TextInput'
 import { UserContext } from '../../contexts/User'
 import { MilestoneManager } from '../../managers/milestone'
+import { DateInput } from '../inputs/DateInput'
 
 
 export const MilestoneView = (props: RouteComponentProps<{ product: string, milestone: string }>) => {
     
     const history = useHistory()
     
-    
     // CONTEXTS
 
     const user = useContext(UserContext)
-
 
     // PARAMS
 
     const productId = props.match.params.product
     const milestoneId = props.match.params.milestone
  
-
     // STATES
 
     // - Entities
     const [product, setProduct] = useState<Product>()
     const [milestone, setMilstone] = useState<Milestone>()
-
-
     // - Values
     const [label, setLabel] = useState<string>('')
-    const [start, setStart] = useState<string>('')
-    const [end, setEnd] = useState<string>('')
+    const [start, setStart] = useState<Date>(new Date())
+    const [end, setEnd] = useState<Date>(new Date())
    
     // EFFECTS
 
     // - Entities
     useEffect(() => { ProductManager.getProduct(productId).then(setProduct) }, [props])
     useEffect(() => { milestoneId != 'new' && MilestoneManager.getMilestone(milestoneId).then(setMilstone) }, [props])
-   
     // - Values
     useEffect(() => { milestone && setLabel(milestone.label) }, [milestone])
-    useEffect(() => { milestone && setStart(milestone.start) }, [milestone])
-    useEffect(() => { milestone && setEnd(milestone.end) }, [milestone])
-
+    useEffect(() => { milestone && setStart(new Date(milestone.start)) }, [milestone])
+    useEffect(() => { milestone && setEnd(new Date(milestone.end)) }, [milestone])
 
     // FUNCTIONS
-
-    
-
 
     async function submitMilestone(event: FormEvent){
         event.preventDefault()
         if(milestoneId == 'new') {
-            await MilestoneManager.addMilestone({userId: user.id, productId: productId, label: label, start: start, end: end})
+            await MilestoneManager.addMilestone({userId: user.id, productId: productId, label: label, start: start.toISOString(), end: end.toISOString()})
             history.replace(`/products/${productId}/milestones/`)
         } else {
-            await MilestoneManager.updateMilestone( milestone.id, { ...milestone, label: label, start: start, end: end})
-            history.goBack
+            await MilestoneManager.updateMilestone( milestone.id, { ...milestone, label: label, start: start.toISOString(), end: end.toISOString()})
+            history.goBack()
         }
     }
-    
 
     // CONSTANTS
-
-    
 
     // RETURN
 
@@ -90,21 +78,17 @@ export const MilestoneView = (props: RouteComponentProps<{ product: string, mile
                             <main className="sidebar">
                                 <div>
                                     <h1>Settings</h1>
-                                    TODO
-
-
                                     <form onSubmit={submitMilestone} onReset={() => history.goBack()}>
                                             <TextInput class='fill' label='Label' placeholder='Type label' value={label} change={setLabel} required />
-                                            <TextInput class='fill' label='Start' placeholder='YYYY-MM-DD' value={start} change={setStart} required />
-                                            <TextInput class='fill' label='End' placeholder='YYYY-MM-DD' value={end} change={setEnd} required />
-                                            <input type='submit' value='Save'/>
+                                            <DateInput label='Start' placeholder='YYYY-MM-DD' value={start} change={setStart} required />
+                                            <DateInput label='End' placeholder='YYYY-MM-DD' value={end} change={setEnd} required />
+                                            <div>
+                                                <div/>
+                                                <div>
+                                                    <input type='submit' value='Save'/>
+                                                </div>
+                                            </div>
                                     </form>
-
-
-
-
-
-
                                 </div>
                                 <div>
                                     <ProductView3D product={product} mouse={true} vr= {true}/>
@@ -116,4 +100,5 @@ export const MilestoneView = (props: RouteComponentProps<{ product: string, mile
             )}
         </main>
     )
+
 }
