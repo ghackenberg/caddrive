46ed855c6c29fbe8256e3366dfa7b883311d0294
Georg Hackenberg <ghackenberg@gmail.com>
2022-04-12 13:13:26 +0200

    common version of http proxy middleware

--- packages/gateway/src/main.ts
+++ packages/gateway/src/main.ts
@@ -3,18 +3,10 @@ import * as proxy from 'http-proxy-middleware'
 
 const app = express()
 
-app.use('/', proxy.createProxyMiddleware({
-    router: {
-        '/rest': 'http://localhost:3001',
-        '/rest-doc': 'http://localhost:3001',
-        '/mqtt': 'http://localhost:3004',
-        '/worker.js': 'http://localhost:3002',
-        '/': 'http://localhost:3003'
-    },
-    pathRewrite: {
-        '/mqtt': '/'
-    },
-    ws: true
-}))
+app.use('/rest', proxy({ target: 'http://localhost:3001' }))
+app.use('/rest-doc', proxy({ target: 'http://localhost:3001' }))
+app.use('/mqtt', proxy({ target: 'http://localhost:3004/', pathRewrite: { '/mqtt': '/' }, ws: true }))
+app.use('/worker.js', proxy({ target: 'http://localhost:3002' }))
+app.use('/', proxy({ target: 'http://localhost:3003' }))
 
 app.listen(3000)
