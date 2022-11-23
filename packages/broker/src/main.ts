d18c6d5b2c39edebccda49211ce5d442bacead61
Georg Hackenberg <ghackenberg@gmail.com>
2022-11-23 22:14:27 +0100

    fix eslint warning in broker

--- packages/broker/src/main.ts
+++ packages/broker/src/main.ts
@@ -8,6 +8,7 @@ const handler = aedes()
 const tcp = net.createServer(handler.handle)
 const web = http.createServer()
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 websocket.createServer({ server: web }, <any> handler.handle)
 
 tcp.listen(1883)
