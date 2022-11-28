ad6d68f15cc53bbd8dd1b02bf1ae44fe83e7b398
Georg Hackenberg <ghackenberg@gmail.com>
2022-11-28 14:27:41 +0100

    update aedes version and add logging

--- packages/broker/src/main.ts
+++ packages/broker/src/main.ts
@@ -1,16 +1,38 @@
-import * as http from 'http'
 import * as net from 'net'
 
-import aedes from 'aedes'
-import * as websocket from 'websocket-stream'
+import Aedes from 'aedes'
+import websocketStream from 'websocket-stream'
 
-const handler = aedes()
+// Set-up MQTT broker
+const aedes = new Aedes()
+aedes.on('client', client => {
+    console.log('Client connect', client.id)
+})
+aedes.on('clientDisconnect', client => {
+    console.log('Client disconnect', client.id)
+})
+aedes.on('clientReady', client => {
+    console.log('Client ready', client.id)
+})
+aedes.on('clientError', (client, error) => {
+    console.error('Client error', client.id, error)
+})
+aedes.on('subscribe', (subscriptions, client) => {
+    console.log('Client subscribe', client.id, subscriptions)
+})
+aedes.on('unsubscribe', (unsubscriptions, client) => {
+    console.log('Client unsubscribe', client.id, unsubscriptions)
+})
 
-const tcp = net.createServer(handler.handle)
-const web = http.createServer()
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-websocket.createServer({ server: web }, <any> handler.handle)
-
+// Handle TCP connections
+const tcp = net.createServer()
+tcp.on('connection', stream => {
+    aedes.handle(stream, null)
+})
 tcp.listen(1883)
-web.listen(3004)
+
+// Handle WebSocket connections
+const web = websocketStream.createServer({ port: 3004 })
+web.on('connection', socket => {
+    aedes.handle(websocketStream(socket), null)
+})
