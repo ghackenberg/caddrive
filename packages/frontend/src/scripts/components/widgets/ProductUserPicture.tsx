78f44588633a158dd749daaec6ec23ed01ba0652
DomFrue <s2110564009@fhooe.at>
2022-11-29 11:53:49 +0100

    fix userPictureWidget to show correct picture after changing userpicture

--- packages/frontend/src/scripts/components/widgets/ProductUserPicture.tsx
+++ packages/frontend/src/scripts/components/widgets/ProductUserPicture.tsx
@@ -14,7 +14,7 @@ export const ProductUserPictureWidget = (props: { user: User, members: Member[],
             ) : (
                 <Fragment>
                     {props.members.map(member => member.userId).includes(props.user.id) ? (
-                        <img src={`/rest/files/${props.user.id}.jpg`} className={props.class}/>
+                        <img src={`/rest/files/${props.user.pictureId}.jpg`} className={props.class}/>
                     ) : (
                         <img src={DiagonalIcon} style={{backgroundImage: `url(/rest/files/${props.user.id}.jpg)`}} className={props.class}/>
                     )}
