46e53db9c7c8a27fb778e01afa33b4e69bcf5f04
Georg Hackenberg <ghackenberg@gmail.com>
2023-01-10 21:05:54 +0100

    make it possible to control backround color

--- packages/frontend/src/scripts/components/widgets/ProductUserPicture.tsx
+++ packages/frontend/src/scripts/components/widgets/ProductUserPicture.tsx
@@ -6,7 +6,7 @@ import * as DiagonalIcon from '/src/images/diagonal.png'
 import * as PixelIcon from '/src/images/pixel.png'
 import * as UserIcon from '/src/images/user.png'
 
-export const ProductUserPictureWidget = (props: { user: User, members: Member[], class?: string }) => {
+export const ProductUserPictureWidget = (props: { user: User, members: Member[], background?: string, class?: string }) => {
     const isDeleted = props.user.deleted
     const isMember = props.members.map(member => member.userId).includes(props.user.id)
 
@@ -18,7 +18,7 @@ export const ProductUserPictureWidget = (props: { user: User, members: Member[],
     const backgroundImage = `url(${props.user.pictureId ? `/rest/files/${props.user.pictureId}.jpg` : UserIcon})`
     const backgroundSize = 'cover'
     const backgroundPosition = 'center'
-    const backgroundColor = 'lightgray'
+    const backgroundColor = props.background || 'lightgray'
 
     const style = { backgroundImage, backgroundSize, backgroundPosition, backgroundColor }
 
--- packages/frontend/src/scripts/components/widgets/UserPicture.tsx
+++ packages/frontend/src/scripts/components/widgets/UserPicture.tsx
@@ -5,7 +5,7 @@ import { User } from 'productboard-common'
 import * as PixelIcon from '/src/images/pixel.png'
 import * as UserIcon from '/src/images/user.png'
 
-export const UserPictureWidget = (props: { user: User, class?: string }) => {
+export const UserPictureWidget = (props: { user: User, background?: string, class?: string }) => {
     const isDeleted = props.user.deleted
 
     const src = isDeleted ? UserIcon : PixelIcon
@@ -16,7 +16,7 @@ export const UserPictureWidget = (props: { user: User, class?: string }) => {
     const backgroundImage = `url(${props.user.pictureId ? `/rest/files/${props.user.pictureId}.jpg` : UserIcon})`
     const backgroundSize = 'cover'
     const backgroundPosition = 'center'
-    const backgroundColor = 'lightgray'
+    const backgroundColor = props.background || 'lightgray'
 
     const style = { backgroundImage, backgroundSize, backgroundPosition, backgroundColor }
 
