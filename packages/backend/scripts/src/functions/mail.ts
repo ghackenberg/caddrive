fdc07bdb4b4aec550711d7cb184333049e54da50
Georg Hackenberg <ghackenberg@gmail.com>
2023-04-27 14:09:44 +0200

    support env smtp server config

--- packages/backend/scripts/src/functions/mail.ts
+++ packages/backend/scripts/src/functions/mail.ts
@@ -1,21 +1,52 @@
 import { createTestAccount, createTransport } from "nodemailer"
 import { nodemailerMjmlPlugin } from "nodemailer-mjml"
 
-export const TRANSPORTER = createTestAccount().then(account => {
-    return createTransport({
-        host: account.smtp.host,
-        port: account.smtp.port,
-        secure: account.smtp.secure,
-        auth: {
-            user: account.user,
-            pass: account.pass
+const SMTP_HOST = process.env['SMTP_HOST']
+const SMTP_PORT = process.env['SMTP_PORT']
+const SMTP_SECURE = process.env['SMTP_SECURE']
+const SMTP_AUTH_USER = process.env['SMTP_AUTH_USER']
+const SMTP_AUTH_PASS = process.env['SMTP_AUTH_PASS']
+
+async function create() {
+    if (SMTP_HOST && SMTP_PORT && SMTP_SECURE && SMTP_AUTH_USER && SMTP_AUTH_PASS) {
+        console.log('Using env SMTP server connection')
+        return createTransport({
+            host: SMTP_HOST,
+            port: parseInt(SMTP_PORT),
+            secure: SMTP_SECURE == 'true',
+            auth: {
+                user: SMTP_AUTH_USER,
+                pass: SMTP_AUTH_PASS
+            }
+        })
+    } else {
+        try {
+            const account = await createTestAccount()
+            console.log('Using test SMTP server connection')
+            return createTransport({
+                host: account.smtp.host,
+                port: account.smtp.port,
+                secure: account.smtp.secure,
+                auth: {
+                    user: account.user,
+                    pass: account.pass
+                }
+            })
+        } catch (error) {
+            console.log('Using stub SMTP server connection')
+            return createTransport({
+                jsonTransport: true
+            })
         }
-    })
-}).catch(() => {
-    return createTransport({
-        jsonTransport: true
-    })
-}).then(transport => {
+    }
+}
+
+async function initialize() {
+    const transport  = await create()
+
     transport.use('compile', nodemailerMjmlPlugin({ templateFolder: 'templates' }))
+
     return transport
-})
+}
+
+export const TRANSPORTER = initialize()
