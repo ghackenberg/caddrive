103428f8c10c9ac0d248dcadd11f33180b7f3538
Georg Hackenberg <ghackenberg@gmail.com>
2023-05-04 16:18:24 +0200

    add data privacy info

--- packages/frontend/src/scripts/components/views/LegalPrivacy.tsx
+++ packages/frontend/src/scripts/components/views/LegalPrivacy.tsx
@@ -8,10 +8,85 @@ export const LegalPrivacyView = () => {
             <main className="view legal privacy">
                 <div>
                     <div>
-                        <h1>Privacy</h1>
+                        <h1>
+                            Privacy
+                        </h1>
                         <p>
-                            Coming soon...
+                            CADDrive stores the following <strong>user data</strong> and <strong>product data</strong>.
+                            You always can request to delete your personal data from our platform.
+                            For such inquiries please send an email to <a href="mailto:mail@caddrive.com">mail@caddrive.com</a>
                         </p>
+                        <h2>
+                            User data
+                        </h2>
+                        <h3>
+                            ... visible to <strong>you only</strong>
+                        </h3>
+                        <ul>
+                            <li>
+                                Your email address
+                            </li>
+                        </ul>
+                        <h3>
+                            ... visible to <strong>all other users</strong>
+                        </h3>
+                        <ul>
+                            <li>
+                                Your profile name
+                            </li>
+                            <li>
+                                Your profile picture
+                            </li>
+                        </ul>
+                        <h2>
+                            Product data
+                        </h2>
+                        <h3>
+                            ... visible to <strong>other product members only</strong>
+                        </h3>
+                        <ul>
+                            <li>
+                                The <strong>private</strong> products, you created
+                            </li>
+                            <li>
+                                The versions of <strong>private</strong> products, you created
+                            </li>
+                            <li>
+                                The issues of <strong>private</strong> products, you created
+                            </li>
+                            <li>
+                                The comments of issues of <strong>private</strong> products, you created
+                            </li>
+                            <li>
+                                The milestones of <strong>private</strong> products, you created
+                            </li>
+                            <li>
+                                The members of <strong>private</strong> products, you created
+                            </li>
+                        </ul>
+                        <h3>
+                            ... visible to <strong>all other users</strong>
+                        </h3>
+                        <ul>
+                            <li>
+                                The <strong>public</strong> products, you created
+                            </li>
+                            <li>
+                                The versions of <strong>public</strong> products, you created
+                            </li>
+                            <li>
+                                The issues of <strong>public</strong> products, you created
+                            </li>
+                            <li>
+                                The comments of issues of <strong>public</strong> products, you created
+                            </li>
+                            <li>
+                                The milestones of <strong>public</strong> products, you created
+                            </li>
+                            <li>
+                                The members of <strong>public</strong> products, you created
+                            </li>
+                        </ul>
                     </div>
                     <LegalFooter replace={true}/>
                 </div>
