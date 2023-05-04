4f65be97eaa929fb98f1de9f61504c819fd4e7c0
Georg Hackenberg <ghackenberg@gmail.com>
2023-05-04 16:49:41 +0200

    finish privacy draft

--- packages/frontend/src/scripts/components/views/LegalPrivacy.tsx
+++ packages/frontend/src/scripts/components/views/LegalPrivacy.tsx
@@ -17,74 +17,134 @@ export const LegalPrivacyView = () => {
                             For such inquiries please send an email to <a href="mailto:mail@caddrive.com">mail@caddrive.com</a>
                         </p>
                         <h2>
-                            User data
+                            1. User data
                         </h2>
                         <h3>
-                            ... visible to <strong>you only</strong>
+                            1.1. ... visible to <strong>you only</strong>
                         </h3>
                         <ul>
                             <li>
-                                Your email address
+                                Your <strong>email address</strong>
                             </li>
                         </ul>
                         <h3>
-                            ... visible to <strong>all other users</strong>
+                            1.2. ... visible to <strong>all other users</strong>
                         </h3>
                         <ul>
                             <li>
-                                Your profile name
+                                Your <strong>profile name</strong>
                             </li>
                             <li>
-                                Your profile picture
+                                Your <strong>profile picture</strong>
                             </li>
                         </ul>
                         <h2>
-                            Product data
+                            2. Product data
                         </h2>
                         <h3>
-                            ... visible to <strong>other product members only</strong>
+                            2.1. ... visible to <strong>other product members only</strong>
                         </h3>
                         <ul>
                             <li>
-                                The <strong>private</strong> products, you created
+                                The <strong><u>private</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>name and description</em>
+                                    </li>
+                                </ul>
                             </li>
                             <li>
-                                The versions of <strong>private</strong> products, you created
+                                The <strong>versions of <u>private</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>date, number, description, model, and image</em>
+                                    </li>
+                                </ul>
                             </li>
                             <li>
-                                The issues of <strong>private</strong> products, you created
+                                The <strong>issues of <u>private</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>date, label, description, audio, milestone, assignees, and state</em>
+                                    </li>
+                                </ul>
                             </li>
                             <li>
-                                The comments of issues of <strong>private</strong> products, you created
+                                The <strong>comments of issues of <u>private</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>date, description, audio, and action</em>
+                                    </li>
+                                </ul>
                             </li>
                             <li>
-                                The milestones of <strong>private</strong> products, you created
+                                The <strong>milestones of <u>private</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>start, end, and name</em>
+                                    </li>
+                                </ul>
                             </li>
                             <li>
-                                The members of <strong>private</strong> products, you created
+                                The <strong>members of <u>private</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>user and role</em>
+                                    </li>
+                                </ul>
                             </li>
                         </ul>
                         <h3>
-                            ... visible to <strong>all other users</strong>
+                            2.2. ... visible to <strong>all other users</strong>
                         </h3>
                         <ul>
                             <li>
-                                The <strong>public</strong> products, you created
+                                The <strong><u>public</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>name and description</em>
+                                    </li>
+                                </ul>
                             </li>
                             <li>
-                                The versions of <strong>public</strong> products, you created
+                                The <strong>versions of <u>public</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>date, number, description, model, and image</em>
+                                    </li>
+                                </ul>
                             </li>
                             <li>
-                                The issues of <strong>public</strong> products, you created
+                                The <strong>issues of <u>public</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>date, label, description, audio, milestone, assignees, and state</em>
+                                    </li>
+                                </ul>
                             </li>
                             <li>
-                                The comments of issues of <strong>public</strong> products, you created
+                                The <strong>comments of issues of <u>public</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>date, description, audio, and action</em>
+                                    </li>
+                                </ul>
                             </li>
                             <li>
-                                The milestones of <strong>public</strong> products, you created
+                                The <strong>milestones of <u>public</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>start, end, and name</em>
+                                    </li>
+                                </ul>
                             </li>
                             <li>
-                                The members of <strong>public</strong> products, you created
+                                The <strong>members of <u>public</u> products</strong> you created including their
+                                <ul>
+                                    <li>
+                                        <em>user and role</em>
+                                    </li>
+                                </ul>
                             </li>
                         </ul>
                     </div>
