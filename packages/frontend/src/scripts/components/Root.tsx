c3064e4baed440f7d102a862647e2a560e547a46
Georg Hackenberg <ghackenberg@gmail.com>
2023-05-04 07:27:45 +0200

    initialize history when loading the app

--- packages/frontend/src/scripts/components/Root.tsx
+++ packages/frontend/src/scripts/components/Root.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { Route, Switch, Redirect, useHistory, useLocation } from 'react-router-dom'
 
 import { importJWK, JWK, jwtVerify, JWTVerifyResult, KeyLike} from 'jose'
 
@@ -28,6 +28,9 @@ const ProductsRouter = React.lazy(() => import('./routers/Products'))
 const UsersRouter = React.lazy(() => import('./routers/Users'))
 
 const Root = () => {
+    const { pathname } = useLocation()
+    const { replace, push } = useHistory()
+
     // STATES
 
     const [publicJWK, setPublicJWK] = React.useState<JWK>()
@@ -40,6 +43,7 @@ const Root = () => {
     const [authContextUser, setAuthContextUser] = React.useState<User>()
     const [contextUser, setContextUser] = React.useState<User>(jwt ? undefined : null)
     const [contextVersion, setContextVersion] = React.useState<Version>()
+    const [initialized, setInitialized] = React.useState(false)
 
     // EFFECTS
 
@@ -64,6 +68,67 @@ const Root = () => {
     React.useEffect(() => {
         userId && UserManager.getUser(userId).then(setContextUser).catch(() => setContextUser(null))
     }, [userId])
+    React.useEffect(() => {
+        if (contextUser !== undefined) {
+            const path = pathname
+            
+            const legal1 = /^\/legal\/(.*)/.exec(path)
+            const legal0 = /^\/legal/.exec(path)
+            
+            const auth1 = /^\/auth\/(.*)/.exec(path)
+            const auth0 = /^\/auth/.exec(path)
+            
+            const users2 = /^\/users\/(.*)\/(.*)/.exec(path)
+            const users1 = /^\/users\/(.*)/.exec(path)
+            const users0 = /^\/users/.exec(path)
+            
+            const products4 = /^\/products\/(.*)\/(.*)\/(.*)\/(.*)/.exec(path)
+            const products3 = /^\/products\/(.*)\/(.*)\/(.*)/.exec(path)
+            const products2 = /^\/products\/(.*)\/(.*)/.exec(path)
+            const products1 = /^\/products\/(.*)/.exec(path)
+
+            if (legal1) {
+                replace('/products')
+                push(`/legal/${legal1[1]}`)
+            } else if (legal0) {
+                replace('/products')
+                push('/legal')
+            } else if (auth1) {
+                replace('/products')
+                push(`/auth/${auth1[1]}`)
+            } else if (auth0) {
+                replace('/products')
+                push('/auth')
+            } else if (users2) {
+                replace('/products')
+                push(`/users/${users2[1]}/${users2[2]}`)
+            } else if (users1) {
+                replace('/products')
+                push(`/users/${users1[1]}`)
+            } else if (users0) {
+                replace('/products')
+                push('/users')
+            } else if (products4) {
+                replace('/products')
+                push(`/products/${products4[1]}/${products4[2]}`)
+                push(`/products/${products4[1]}/${products4[2]}/${products4[3]}/${products4[4]}`)
+            } else if (products3) {
+                replace('/products')
+                push(`/products/${products3[1]}/${products3[2]}`)
+                push(`/products/${products3[1]}/${products3[2]}/${products3[3]}`)
+            } else if (products2) {
+                replace('/products')
+                push(`/products/${products2[1]}/${products2[2]}`)
+            } else if (products1) {
+                replace('/products')
+                push(`/products/${products1[1]}`)
+            } else {
+                replace('/products')
+            }
+
+            setInitialized(true)
+        }
+    }, [contextUser])
 
     // FUNCTIONS
     
@@ -93,29 +158,27 @@ const Root = () => {
     // RETURN
 
     return (
-        <BrowserRouter>
-            <AuthContext.Provider value={{ authContextToken, setAuthContextToken, authContextUser, setAuthContextUser }}>
-                <UserContext.Provider value={{ contextUser, setContextUser: intercept }}>
-                    <VersionContext.Provider value={{ contextVersion, setContextVersion }}>
-                        <PageHeaderRoot/>
-                        {contextUser === undefined ? (
-                            <LoadingView/>
-                        ) : (
-                            <React.Suspense fallback={<LoadingView/>}>
-                                <Switch>
-                                    <Route path="/legal" component={LegalRouter}/>
-                                    <Route path="/auth" component={AuthRouter}/>
-                                    <Route path="/users" component={UsersRouter}/>
-                                    <Route path="/products" component={ProductsRouter}/>
-                                    <Redirect path="/" exact to="/products" push={false}/>
-                                    <Route component={MissingView}/>
-                                </Switch>
-                            </React.Suspense>
-                        )}
-                    </VersionContext.Provider>
-                </UserContext.Provider>
-            </AuthContext.Provider>
-        </BrowserRouter>
+        <AuthContext.Provider value={{ authContextToken, setAuthContextToken, authContextUser, setAuthContextUser }}>
+            <UserContext.Provider value={{ contextUser, setContextUser: intercept }}>
+                <VersionContext.Provider value={{ contextVersion, setContextVersion }}>
+                    <PageHeaderRoot/>
+                    {initialized ? (
+                        <React.Suspense fallback={<LoadingView/>}>
+                            <Switch>
+                                <Route path="/legal" component={LegalRouter}/>
+                                <Route path="/auth" component={AuthRouter}/>
+                                <Route path="/users" component={UsersRouter}/>
+                                <Route path="/products" component={ProductsRouter}/>
+                                <Redirect path="/" exact to="/products" push={false}/>
+                                <Route component={MissingView}/>
+                            </Switch>
+                        </React.Suspense>
+                    ) : (
+                        <LoadingView/>
+                    )}
+                </VersionContext.Provider>
+            </UserContext.Provider>
+        </AuthContext.Provider>
     )
 }
 
