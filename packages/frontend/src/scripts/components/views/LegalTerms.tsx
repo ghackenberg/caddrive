ad048e03966b5eb6b2e9a3a5a8fc1dc877486bc7
Georg Hackenberg <ghackenberg@gmail.com>
2023-05-10 20:17:24 +0200

    continue working on terms

--- packages/frontend/src/scripts/components/views/LegalTerms.tsx
+++ packages/frontend/src/scripts/components/views/LegalTerms.tsx
@@ -8,22 +8,29 @@ export const LegalTermsView = () => {
             <div>
                 <div>
                     <h1>Terms</h1>
-                    <h2>1. Our obligations</h2>
+                    <h2>1. Our rights and obligations</h2>
                     <ol>
                         <li>
                             <em>Coming soon...</em>
                         </li>
                     </ol>
-                    <h2>2. Your obligations</h2>
+                    <h2>2. Your rights and obligations</h2>
+                    <h3>2.1 Offensive data</h3>
                     <ol>
                         <li>
-                            You must not upload data (e.g. texts, images, sounds, and models) to our platform, for which you have no copyright.
+                            You <strong>must not</strong> upload data (e.g. texts, images, sounds, and models) to our platform, which is offensive to other people.
+                        </li>
+                    </ol>
+                    <h3>2.2 Data copyright</h3>
+                    <ol>
+                        <li>
+                            You <strong>must not</strong> upload data (e.g. texts, images, sounds, and models) to our platform, for which you have no copyright.
                         </li>
                         <li>
-                            You must not upload data (e.g. texts, images, sounds, and models) to our platform, for which you have no right to transfer copyright to us permanently.
+                            You <strong>must not</strong> upload data (e.g. texts, images, sounds, and models) to our platform, for which you have no right to transfer copyright to us permanently.
                         </li>
                         <li>
-                            When you upload data (e.g. texts, images, sounds, and models) to our platform, you transfer copyright for this data to us permanently.
+                            By uploading data (e.g. texts, images, sounds, and models) to our platform, you <strong>transfer copyright</strong> for this data to us permanently.
                         </li>
                     </ol>
                 </div>
