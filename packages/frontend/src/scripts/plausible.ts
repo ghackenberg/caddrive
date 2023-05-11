711f29ed3e65acf4ddcae1367b5990015e72698d
Georg Hackenberg <ghackenberg@gmail.com>
2023-05-11 09:44:30 +0200

    fix tracker params

--- packages/frontend/src/scripts/plausible.ts
+++ packages/frontend/src/scripts/plausible.ts
@@ -1,8 +1,8 @@
 import Plausible from 'plausible-tracker'
 
 export const plausible = Plausible({
-    domain: 'https://analytics.mindtastic.ai',
-    apiHost: 'caddrive.com'
+    domain: 'caddrive.com',
+    apiHost: 'https://analytics.mindtastic.ai'
 })
 
 plausible.enableAutoPageviews()
