2d2ac7fe699953177d2dc94475eb56c3221d32b5
DomFrue <s2110564009@fhooe.at>
2023-05-24 17:19:01 +0200

    remove mqtt from modules

--- packages/backend/scripts/src/modules/rest/tagAssignments/tagAssignment.module.ts
+++ packages/backend/scripts/src/modules/rest/tagAssignments/tagAssignment.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
-import { ClientsModule, Transport } from '@nestjs/microservices'
 
 import { TagAssignmentController } from './tagAssignment.controller'
 import { TagAssignmentService } from './tagAssignment.service'
 
 @Module({
-    imports: [ClientsModule.register([{ name: 'MQTT', transport: Transport.MQTT }])],
     controllers: [TagAssignmentController],
     providers: [TagAssignmentService],
     exports: [TagAssignmentService],
