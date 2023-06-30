02fa0e39a7daa3f7a1aa71a25e0cd8c15eee19ad
Georg Hackenberg <ghackenberg@gmail.com>
2023-06-30 16:40:16 +0200

    make model rendering more robust

--- packages/backend/scripts/src/modules/rest/versions/version.service.ts
+++ packages/backend/scripts/src/modules/rest/versions/version.service.ts
@@ -41,9 +41,10 @@ export class VersionService implements VersionREST<VersionAddData, VersionUpdate
         const created = Date.now()
         const updated = created
         const userId = this.request.user.id
-        const modelType = await this.processModel(id, null, files)
-        const imageType = await this.processImage(id, null, files)
+        const modelType = await processModel(id, null, files)
+        const imageType = await processImage(id, null, files)
         const version = await Database.get().versionRepository.save({ id, created, updated, userId, modelType, imageType, ...data })
+        renderImage(id, files)
         return convertVersion(version)
     }
 
@@ -59,9 +60,10 @@ export class VersionService implements VersionREST<VersionAddData, VersionUpdate
         version.minor = data.minor
         version.patch = data.patch
         version.description = data.description
-        version.modelType = await this.processModel(id, version.modelType, files)
-        version.imageType = await this.processImage(id, version.imageType, files)
+        version.modelType = await processModel(id, version.modelType, files)
+        version.imageType = await processImage(id, version.imageType, files)
         await Database.get().versionRepository.save(version)
+        renderImage(id, files)
         return convertVersion(version)
     }
 
@@ -72,83 +74,100 @@ export class VersionService implements VersionREST<VersionAddData, VersionUpdate
         await Database.get().versionRepository.save(version)
         return convertVersion(version)
     }
+}
 
-    private async processModel(id: string, modelType: 'glb' | 'ldr' | 'mpd', files?: {model: Express.Multer.File[], image: Express.Multer.File[]}) {
-        if (files) {
-            if (files.model) {
-                if (files.model.length == 1) {
-                    if (files.model[0].originalname.endsWith('.glb')) {
-                        writeFileSync(`./uploads/${id}.glb`, files.model[0].buffer)
-                        return 'glb'
-                    } else if (files.model[0].originalname.endsWith('.ldr')) {
-                        writeFileSync(`./uploads/${id}.ldr`, files.model[0].buffer)
-                        return 'ldr'
-                    } else if (files.model[0].originalname.endsWith('.mpd')) {
-                        writeFileSync(`./uploads/${id}.mpd`, files.model[0].buffer)
-                        return 'mpd'
-                    } else {
-                        throw new HttpException('Model file type not supported.', 400)
-                    }
+async function processModel(id: string, modelType: 'glb' | 'ldr' | 'mpd', files?: {model: Express.Multer.File[], image: Express.Multer.File[]}) {
+    if (files) {
+        if (files.model) {
+            if (files.model.length == 1) {
+                if (files.model[0].originalname.endsWith('.glb')) {
+                    writeFileSync(`./uploads/${id}.glb`, files.model[0].buffer)
+                    return 'glb'
+                } else if (files.model[0].originalname.endsWith('.ldr')) {
+                    writeFileSync(`./uploads/${id}.ldr`, files.model[0].buffer)
+                    return 'ldr'
+                } else if (files.model[0].originalname.endsWith('.mpd')) {
+                    writeFileSync(`./uploads/${id}.mpd`, files.model[0].buffer)
+                    return 'mpd'
                 } else {
-                    throw new HttpException('Only one model file supported.', 400)
+                    throw new HttpException('Model file type not supported.', 400)
                 }
             } else {
-                throw new HttpException('Model file must be provided.', 400)
+                throw new HttpException('Only one model file supported.', 400)
             }
         } else {
-            if (modelType) {
-                return modelType
-            } else {
-                throw new HttpException('Model file must be provided.', 400)
-            }
+            throw new HttpException('Model file must be provided.', 400)
+        }
+    } else {
+        if (modelType) {
+            return modelType
+        } else {
+            throw new HttpException('Model file must be provided.', 400)
         }
     }
-    
-    private async processImage(id: string, imageType: 'png', files?: {model: Express.Multer.File[], image: Express.Multer.File[]}) {
-        if (files) {
-            if (files.image) {
-                if (files.image.length == 1) {
-                    if (files.image[0].mimetype == 'image/png') {
-                        writeFileSync(`./uploads/${id}.png`, files.image[0].buffer)
-                        return 'png'
-                    } else {
-                        throw new HttpException('Image file type not supported.', 400)
-                    }
+}
+
+async function processImage(id: string, imageType: 'png', files?: {model: Express.Multer.File[], image: Express.Multer.File[]}) {
+    if (files) {
+        if (files.image) {
+            if (files.image.length == 1) {
+                if (files.image[0].mimetype == 'image/png') {
+                    writeFileSync(`./uploads/${id}.png`, files.image[0].buffer)
+                    return 'png'
                 } else {
-                    throw new HttpException('Only one image file supported.', 400)
-                }
-            } else if (files.model) {
-                if (files.model.length == 1) {
-                    if (files.model[0].originalname.endsWith('.glb')) {
-                        renderGlb(files.model[0].buffer, 1000, 1000).then(image => this.updateImage(id, image)).catch(console.error)
-                    } else if (files.model[0].originalname.endsWith('.ldr')) {
-                        renderLDraw(files.model[0].buffer.toString(), 1000, 1000).then(image => this.updateImage(id, image)).catch(console.error)
-                    } else if (files.model[0].originalname.endsWith('.mpd')) {
-                        renderLDraw(files.model[0].buffer.toString(), 1000, 1000).then(image => this.updateImage(id, image)).catch(console.error)
-                    } else {
-                        throw new HttpException('Model file type not supported.', 400)
-                    }
-                    return null
-                } else {
-                    throw new HttpException('Only one model file supported.', 400)
+                    throw new HttpException('Image file type not supported.', 400)
                 }
             } else {
-                throw new HttpException('Image or model file must be provided.', 400)
+                throw new HttpException('Only one image file supported.', 400)
+            }
+        } else if (files.model) {
+            // Render model later
+            return null
+        } else {
+            throw new HttpException('Image or model file must be provided.', 400)
+        }
+    } else {
+        if (imageType) {
+            return imageType
+        } else {
+            throw new HttpException('Image or model file must be provided.', 400)
+        }
+    }
+}
+
+async function renderImage(id: string, files?: {model: Express.Multer.File[], image: Express.Multer.File[]}) {
+    if (files) {
+        if (files.image) {
+            // Model must not be rendered
+        } else if (files.model) {
+            if (files.model.length == 1) {
+                if (files.model[0].originalname.endsWith('.glb')) {
+                    const image = await renderGlb(files.model[0].buffer, 1000, 1000)
+                    await updateImage(id, image)
+                } else if (files.model[0].originalname.endsWith('.ldr')) {
+                    const image = await renderLDraw(files.model[0].buffer.toString(), 1000, 1000)
+                    await updateImage(id, image)
+                } else if (files.model[0].originalname.endsWith('.mpd')) {
+                    const image = await renderLDraw(files.model[0].buffer.toString(), 1000, 1000)
+                    await updateImage(id, image)
+                } else {
+                    throw new HttpException('Model file type not supported.', 400)
+                }
+            } else {
+                throw new HttpException('Only one model file supported.', 400)
             }
         } else {
-            if (imageType) {
-                return imageType
-            } else {
-                throw new HttpException('Image or model file must be provided.', 400)
-            }
+            throw new HttpException('Image or model file must be provided.', 400)
         }
     }
-    
-    private async updateImage(id: string, image: Jimp) {
-        await image.writeAsync(`./uploads/${id}.png`)
-        const version = await Database.get().versionRepository.findOneBy({ id })
-        version.updated = Date.now()
-        version.imageType = 'png'
-        await Database.get().versionRepository.save(version)
-    }
+}
+
+async function updateImage(id: string, image: Jimp) {
+    // Save image
+    await image.writeAsync(`./uploads/${id}.png`)
+    // Update version
+    const version = await Database.get().versionRepository.findOneBy({ id })
+    version.updated = Date.now()
+    version.imageType = 'png'
+    await Database.get().versionRepository.save(version)
 }
