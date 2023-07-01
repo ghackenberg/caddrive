a5d7151f946ebf64ebb184b4fc68b945e2d6fbf6
Georg Hackenberg <ghackenberg@gmail.com>
2023-07-01 08:41:40 +0200

    revise strategy for access control

--- packages/broker/scripts/src/main.ts
+++ packages/broker/scripts/src/main.ts
@@ -8,18 +8,23 @@ import { exec } from 'mqtt-pattern'
 import { IsNull } from 'typeorm'
 import { createWebSocketStream, WebSocketServer } from 'ws'
 
+import { Member, Product } from 'productboard-common'
 import { Database } from 'productboard-database'
 
 // Variables
 
-let PUBLIC_KEY: KeyLike | Uint8Array // Load JWT public key from Nest.js backend later!
+let JWT_PUBLIC_KEY: KeyLike | Uint8Array // Load JWT public key from Nest.js backend later!
 
 // Constants
 
 const NET_PORT = 3003
 const HTTP_PORT = 3004
 
-const USER_IDS: { [clientId: string]: string } = {} // Remember user ID of each MQTT client!
+const CLIENT_USER_IDS: { [clientId: string]: string } = {} // Remember user ID of each MQTT client!
+
+const PRODUCT_CACHE: { [productId: string]: boolean } = {}
+
+const PRODUCT_MEMBER_CACHE: { [productId: string]: string[] }= {}
 
 // Constants - MQTT Broker
 
@@ -32,21 +37,21 @@ aedes.authenticate = async (client, username, _password, callback) => {
         console.log('authenticate', client.id)
         if (username) {
             // Load JWT public key from Nest.js backend if necessary!
-            if (!PUBLIC_KEY) {
+            if (!JWT_PUBLIC_KEY) {
                 const request = axios.get<JWK>('http://localhost:3001/rest/keys')
                 const response = await request
-                PUBLIC_KEY = await importJWK(response.data, "PS256")
+                JWT_PUBLIC_KEY = await importJWK(response.data, "PS256")
             }
             // Verify JWT
-            const result = await jwtVerify(username, PUBLIC_KEY)
+            const result = await jwtVerify(username, JWT_PUBLIC_KEY)
             // Remember user ID of MQTT client!
             const payload = result.payload as { userId: string }
             const userId = payload.userId
-            USER_IDS[client.id] = userId
+            CLIENT_USER_IDS[client.id] = userId
             // Success
             callback(null, true)
         } else {
-            USER_IDS[client.id] = null
+            CLIENT_USER_IDS[client.id] = null
             callback(null, true)
         }
     } catch (e) {
@@ -59,41 +64,97 @@ aedes.authenticate = async (client, username, _password, callback) => {
 aedes.authorizeSubscribe = async (client, subscription, callback) => {
     try {
         console.log('authorizeSubscribe', client.id, subscription.topic)
-        // User topics are fine for everybody
-        const userMatch = exec('/users/+id', subscription.topic)
+        // User topics are visible to everybody
+        const userMatch = exec('/users/+userId', subscription.topic)
         if (userMatch) {
             return callback(null, subscription)
         }
         // Product topics have to be checked more carefully
         const productMatch = exec('/products/#path', subscription.topic)
         if (productMatch) {
-            // Public product topics are fine for everybody again
-            const id = productMatch.path[0]
-            const product = await Database.get().productRepository.findOneByOrFail({ id, deleted: IsNull() })
-            if (product.public) {
+            // Load data
+            const productId = productMatch.path[0]
+            // Load product
+            if (!(productId in PRODUCT_CACHE)) {
+                const product = await Database.get().productRepository.findOneByOrFail({ id: productId, deleted: IsNull() })
+                PRODUCT_CACHE[product.id] = product.public
+            }
+            // Load product members
+            if (!(productId in PRODUCT_MEMBER_CACHE)) {
+                const members = await Database.get().memberRepository.findBy({ productId, deleted: IsNull() })
+                PRODUCT_MEMBER_CACHE[productId] = members.map(member => member.userId)
+            }
+            // Check permission
+            const userId = CLIENT_USER_IDS[client.id]
+            // Public product topics are visible to everybody
+            if (PRODUCT_CACHE[productId]) {
                 return callback(null, subscription)
             }
-            // Non-public product topics require product membership
-            await Database.get().memberRepository.findOneByOrFail({ productId: id, userId: USER_IDS[client.id], deleted: IsNull() })
-            return callback(null, subscription)
+            // Non-public product topics are visible to product members only
+            if (PRODUCT_MEMBER_CACHE[productId].indexOf(userId) != -1) {
+                return callback(null, subscription)
+            }
+            return callback(new Error('Product not allowed!'), null)
         }
         // Other topics do not exist
-        callback(new Error('Topic not supported!'), null)
+        return callback(new Error('Topic not supported!'), null)
     } catch (e) {
-        callback(e, null)
+        return callback(e, null)
     }
 }
 aedes.authorizePublish = async (client, packet, callback) => {
     console.log('authorizePublish', client.id, packet.topic)
-    if (USER_IDS[client.id] == 'backend') {
+    // Only backend can publish messages!
+    if (CLIENT_USER_IDS[client.id] == 'backend') {
+        // Allow publishing
         callback(null)
+        // Update product cache
+        const productMatch = exec('/products/+productId', packet.topic)
+        if (productMatch) {
+            const productId = productMatch.productId
+            const product = JSON.parse(packet.payload.toString()) as Product
+            PRODUCT_CACHE[productId] = product.public
+        }
+        // Update product member cache
+        const memberMatch = exec('/products/+productId/members/+memberId', packet.topic)
+        if (memberMatch) {
+            const productId = memberMatch.productId
+            if (productId in PRODUCT_MEMBER_CACHE) {
+                const member = JSON.parse(packet.payload.toString()) as Member
+                if (member.deleted) {
+                    const userId = member.userId
+                    PRODUCT_MEMBER_CACHE[productId].splice(PRODUCT_MEMBER_CACHE[productId].indexOf(userId), 1)
+                }
+            }
+        }
     } else {
         callback(new Error('Only backend can publish!'))
     }
 }
 aedes.authorizeForward = (client, packet) => {
     console.log('authorizeForward', client.id, packet.topic)
-    return packet
+    // User topics can be forwarded without further checks
+    const userMatch = exec('/products/+userId', packet.topic)
+    if (userMatch) {
+        return packet
+    }
+    // Product topics have to be checked more carefully
+    const productMatch = exec('/products/#path', packet.topic)
+    if (productMatch) {
+        // Check product cache
+        const productId = productMatch.path[0]
+        if (PRODUCT_CACHE[productId]) {
+            return packet
+        }
+        // Check product member cache
+        const userId = CLIENT_USER_IDS[client.id]
+        if (PRODUCT_MEMBER_CACHE[productId].indexOf(userId) != -1) {
+            return packet
+        }
+        return null
+    }
+    // Other topics do not exist
+    return null
 }
 
 // Constants - MQTT Broker - Events
@@ -109,7 +170,7 @@ aedes.on('publish', (packet, client) => {
 })
 aedes.on('clientDisconnect', client => {
     console.log('clientDisconnect', client.id)
-    delete USER_IDS[client.id]
+    delete CLIENT_USER_IDS[client.id]
 })
 
 // Contants - Net server
