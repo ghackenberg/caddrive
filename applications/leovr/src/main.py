c7bc06fee64e4083ecf0046613f68239e615761c
Georg Hackenberg <ghackenberg@gmail.com>
2024-02-07 22:40:16 +0100

    refactor cube drawing and introduce line drawing

--- applications/leovr/src/main.py
+++ applications/leovr/src/main.py
@@ -2,7 +2,7 @@ from direct.showbase.ShowBase import ShowBase
 
 from panda3d.core import GeomNode
 
-from caddrive.visualization.panda3d.util import makeSquare
+from caddrive.visualization.panda3d.util import makeCube
 
 class LeoVR(ShowBase):
 
@@ -10,24 +10,12 @@ class LeoVR(ShowBase):
 
         super().__init__(self)
 
-        geom0 = makeSquare(-1, -1, -1, 1, -1, 1)
-        geom1 = makeSquare(-1, 1, -1, 1, 1, 1)
-        geom2 = makeSquare(-1, 1, 1, 1, -1, 1)
-        geom3 = makeSquare(-1, 1, -1, 1, -1, -1)
-        geom4 = makeSquare(-1, -1, -1, -1, 1, 1)
-        geom5 = makeSquare(1, -1, -1, 1, 1, 1)
+        node = makeCube()
 
-        node = GeomNode('square')
-        node.addGeom(geom0)
-        node.addGeom(geom1)
-        node.addGeom(geom2)
-        node.addGeom(geom3)
-        node.addGeom(geom4)
-        node.addGeom(geom5)
-
-        path = self.render.attachNewNode(node)
-        path.setTwoSided(True)
-        path.setHpr(0, -45, 45)
+        nodePath = self.render.attachNewNode(node)
+        nodePath.setRenderModeThickness(2)
+        nodePath.setTwoSided(True)
+        nodePath.setHpr(0, -45, 45)
 
         self.disableMouse()
         self.camera.setPos(0, -20, 0)
--- packages/python/src/caddrive/visualization/panda3d/util.py
+++ packages/python/src/caddrive/visualization/panda3d/util.py
@@ -7,7 +7,7 @@ def normalized(x: float, y: float, z: float):
 
     return myVec
 
-def makeSquare(x1: float, y1: float, z1: float, x2: float, y2: float, z2: float):
+def makeQuadFill(x1: float, y1: float, z1: float, x2: float, y2: float, z2: float):
     
     from panda3d.core import GeomVertexFormat
     from panda3d.core import GeomVertexData
@@ -45,10 +45,10 @@ def makeSquare(x1: float, y1: float, z1: float, x2: float, y2: float, z2: float)
         normal.addData3(normalized(2 * x2 - 1, 2 * y2 - 1, 2 * z2 - 1))
         normal.addData3(normalized(2 * x1 - 1, 2 * y1 - 1, 2 * z2 - 1))
 
-    color.addData4f(1.0, 0.0, 0.0, 1.0)
-    color.addData4f(0.0, 1.0, 0.0, 1.0)
-    color.addData4f(0.0, 0.0, 1.0, 1.0)
-    color.addData4f(1.0, 0.0, 1.0, 1.0)
+    color.addData4f(0.5, 0.0, 0.0, 1.0)
+    color.addData4f(0.0, 0.5, 0.0, 1.0)
+    color.addData4f(0.0, 0.0, 0.5, 1.0)
+    color.addData4f(0.5, 0.0, 0.5, 1.0)
 
     texcoord.addData2f(0.0, 1.0)
     texcoord.addData2f(0.0, 0.0)
@@ -63,3 +63,81 @@ def makeSquare(x1: float, y1: float, z1: float, x2: float, y2: float, z2: float)
     geom.addPrimitive(triangles)
 
     return geom
+
+def makeQuadLine(x1: float, y1: float, z1: float, x2: float, y2: float, z2: float):
+    
+    from panda3d.core import GeomVertexFormat
+    from panda3d.core import GeomVertexData
+    from panda3d.core import GeomVertexWriter
+    from panda3d.core import GeomLines
+    from panda3d.core import Geom
+
+    format = GeomVertexFormat.getV3n3cpt2()
+    data = GeomVertexData('square', format, Geom.UHDynamic)
+
+    vertex = GeomVertexWriter(data, 'vertex')
+    color = GeomVertexWriter(data, 'color')
+
+    if x1 != x2:
+        vertex.addData3(x1, y1, z1)
+        vertex.addData3(x2, y1, z1)
+        vertex.addData3(x2, y2, z2)
+        vertex.addData3(x1, y2, z2)
+
+    else:
+        vertex.addData3(x1, y1, z1)
+        vertex.addData3(x2, y2, z1)
+        vertex.addData3(x2, y2, z2)
+        vertex.addData3(x1, y1, z2)
+
+    color.addData4f(1.0, 0.0, 0.0, 1.0)
+    color.addData4f(0.0, 1.0, 0.0, 1.0)
+    color.addData4f(0.0, 0.0, 1.0, 1.0)
+    color.addData4f(1.0, 0.0, 1.0, 1.0)
+
+    lines = GeomLines(Geom.UHDynamic)
+    lines.addVertices(0, 1)
+    lines.addVertices(1, 2)
+    lines.addVertices(2, 3)
+    lines.addVertices(3, 0)
+
+    geom = Geom(data)
+    geom.addPrimitive(lines)
+
+    return geom
+
+def makeCube():
+    
+    from panda3d.core import GeomNode
+
+    quadLine0 = makeQuadLine(-1, -1, -1, 1, -1, 1)
+    quadLine1 = makeQuadLine(-1, 1, -1, 1, 1, 1)
+    quadLine2 = makeQuadLine(-1, 1, 1, 1, -1, 1)
+    quadLine3 = makeQuadLine(-1, 1, -1, 1, -1, -1)
+    quadLine4 = makeQuadLine(-1, -1, -1, -1, 1, 1)
+    quadLine5 = makeQuadLine(1, -1, -1, 1, 1, 1)
+
+    quadFill0 = makeQuadFill(-1, -1, -1, 1, -1, 1)
+    quadFill1 = makeQuadFill(-1, 1, -1, 1, 1, 1)
+    quadFill2 = makeQuadFill(-1, 1, 1, 1, -1, 1)
+    quadFill3 = makeQuadFill(-1, 1, -1, 1, -1, -1)
+    quadFill4 = makeQuadFill(-1, -1, -1, -1, 1, 1)
+    quadFill5 = makeQuadFill(1, -1, -1, 1, 1, 1)
+
+    node = GeomNode('square')
+    
+    node.addGeom(quadLine0)
+    node.addGeom(quadLine1)
+    node.addGeom(quadLine2)
+    node.addGeom(quadLine3)
+    node.addGeom(quadLine4)
+    node.addGeom(quadLine5)
+
+    node.addGeom(quadFill0)
+    node.addGeom(quadFill1)
+    node.addGeom(quadFill2)
+    node.addGeom(quadFill3)
+    node.addGeom(quadFill4)
+    node.addGeom(quadFill5)
+
+    return node
