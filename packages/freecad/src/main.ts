854a7b97658eb716fee565e2e3ef643c86883988
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2024-10-09 18:20:33 +0200

    speed up brep meshing with lower quality

--- packages/freecad/src/main.ts
+++ packages/freecad/src/main.ts
@@ -28,16 +28,16 @@ export class FreeCADObject {
     public label: string
     public visibility: boolean
 
-    public shape_file: string
+    public brep: string
 
     constructor(public name: string, public type: string) {}
 
-    hasShapeBRep() {
-        if (this.shape_file) {
+    isVisible() {
+        if (this.brep) {
             return this.visibility === undefined || this.visibility
         } else {
             for (const child of this.group || []) {
-                if (child.hasShapeBRep()) {
+                if (child.isVisible()) {
                     return true
                 }
             }
@@ -56,26 +56,28 @@ function traverse(object: Object3D, material: MeshStandardMaterial) {
 }
 
 export async function parseFCStdModel(data: ReadableStream | BlobReader, brep2Glb: (content: string) => Promise<Uint8Array>) {
-    const diffuse: {[name: string]: MeshStandardMaterial[]} = {}
+    // Data stuctures
+    const colors: {[name: string]: MeshStandardMaterial[]} = {}
     const breps: {[name: string]: Entry} = {}
+    const gltfs: {[name: string]: GLTF} = {}
+    // Main document
     let doc: FreeCADDocument
+    // Read ZIP file
     const reader = new ZipReader(data)
-    const parser = new DOMParser()
-    // Read files in ZIP archive
     const entries = await reader.getEntries()
     for (const entry of entries) {
-        //console.log(entry.filename)
-        // Check file type
         if (entry.filename == 'Document.xml') {
-            // Parse XML file
+            // Read main document
             const writer = new TextWriter()
             const content = await entry.getData(writer)
+            const parser = new DOMParser()
             const document = parser.parseFromString(content, 'application/xml')
             doc = parseFCStdDocument(document)
         } else if (entry.filename.startsWith('DiffuseColor')) {
+            // Read color specification
             const writer = new Uint8ArrayWriter()
             const content = await entry.getData(writer)
-            diffuse[entry.filename] = []
+            colors[entry.filename] = []
             for (let i = 1; i < content.length / 4; i++) {
                 const a = 1 - content[i * 4 + 0] / 255
                 const b = 1 - content[i * 4 + 1] / 255
@@ -83,9 +85,10 @@ export async function parseFCStdModel(data: ReadableStream | BlobReader, brep2Gl
                 const r = 1 - content[i * 4 + 3] / 255
                 const color = new Color(r, g, b)
                 const material = new MeshStandardMaterial({ color, opacity: a })
-                diffuse[entry.filename].push(material)
+                colors[entry.filename].push(material)
             }
         } else if (entry.filename.endsWith('.brp')) {
+            // Remember brep entry
             breps[entry.filename] = entry
         }
     }
@@ -96,57 +99,59 @@ export async function parseFCStdModel(data: ReadableStream | BlobReader, brep2Gl
             delete doc.objects[object.name]
         }
     }
-    // Log result
-    console.log(doc)
     // Convert to THREEJS
     const model = new Group()
     model.name = doc.label
     model.rotateX(-Math.PI / 2)
     for (const obj of Object.values(doc.objects)) {
-        if (obj.hasShapeBRep()) {
-            model.add(await convertFCObject(obj, diffuse, breps, brep2Glb))
+        if (obj.isVisible()) {
+            model.add(await convertFCObject(obj, colors, breps, gltfs, brep2Glb))
         }
     }
     return model
 }
 
-async function convertFCObject(obj: FreeCADObject, diffuse: {[name: string]: MeshStandardMaterial[]}, breps: {[name: string]: Entry}, brep2Glb: (content: string) => Promise<Uint8Array>) {
+async function convertFCObject(obj: FreeCADObject, colors: {[name: string]: MeshStandardMaterial[]}, breps: {[name: string]: Entry}, gltfs: {[name: string]: GLTF}, brep2Glb: (content: string) => Promise<Uint8Array>) {
     const container = new Group()
     container.name = obj.label
-
-    if (obj.shape_file) {
+    if (obj.brep) {
         try {
-            // Parse BRep file
-            const entry = breps[obj.shape_file]
-            const writer = new TextWriter()
-            const content = await entry.getData(writer)
-            console.log('Parsing', entry.filename)
-            const glbFileData = await brep2Glb(content)
-            const gltf = await new Promise<GLTF>((resolve, reject) => {
-                GLTF.parse(glbFileData.buffer, undefined, resolve, reject)
-            })
-            const clone = gltf.scene.clone(true)
-            // Update mesh materials
-            const other = entry.filename.replace('PartShape', 'DiffuseColor').replace('.brp', '')
-            if (other in diffuse) {
-                const material = diffuse[other][0]
-                traverse(gltf.scene, material)
+            const file = obj.brep
+            // Parse brep
+            if (!(file in gltfs)) {
+                const entry = breps[obj.brep]
+                const writer = new TextWriter()
+                const content = await entry.getData(writer)
+                console.log('Converting', file)
+                const data = await brep2Glb(content)
+                console.log('Parsing', file)
+                gltfs[file] = await new Promise<GLTF>((resolve, reject) => {
+                    GLTF.parse(data.buffer, undefined, resolve, reject)
+                })
             }
-            traverse(clone, new MeshStandardMaterial({ color: 'black', wireframe: true }))
+            // Clone scene
+            const face = gltfs[file].scene.clone(true)
+            const wire = gltfs[file].scene.clone(true)
+            // Update mesh materials
+            const other = file.replace('PartShape', 'DiffuseColor').replace('.brp', '')
+            if (other in colors) {
+                const material = colors[other][0]
+                traverse(face, material)
+            }
+            traverse(wire, new MeshStandardMaterial({ color: 'black', wireframe: true }))
             // Add scene objects
-            container.add(clone)
-            container.add(gltf.scene)
+            container.add(face)
+            container.add(wire)
         } catch (e) {
             console.log(e)
         }
     } else if (obj.group) {
         for (const child of obj.group) {
-            if (child.hasShapeBRep()) {
-                container.add(await convertFCObject(child, diffuse, breps, brep2Glb))
+            if (child.isVisible()) {
+                container.add(await convertFCObject(child, colors, breps, gltfs, brep2Glb))
             }
         }
     }
-
     return container
 }
 
@@ -226,7 +231,7 @@ function parseFCStdDocumentObjectProperty(data: Element, obj: FreeCADObject, doc
         } else if (name == 'Shape') {
             if (type == 'Part::PropertyPartShape') {
                 const child = data.getElementsByTagName('Part')[0]
-                obj.shape_file = child.getAttribute('file')
+                obj.brep = child.getAttribute('file')
             } else if (type == 'App::PropertyLink') {
                 const child = data.getElementsByTagName('LinkSub')[0]
                 const other = doc.objects[child.getAttribute('value')]
